b83df5c8db481f95475a223c03a1389b8ac071d8
lisandrogarciarabal <81982875+lisandrogarciarabal@users.noreply.github.com>
2024-09-20 16:01:47 -0300

    feat: subiendo actividad 1

--- actividades/ej_01/app.py
+++ actividades/ej_01/app.py
@@ -1,1 +1,12 @@
 # Aplicacion del servidor
+from microdot import Microdot, send_file
+
+app = Microdot()
+
+from microdot import send_file
+
+@app.get('/')
+async def index(request):
+    return send_file('/index.html')
+
+app.run(port = 80)
